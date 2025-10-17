b9fe3cd1400422d2e593c76f26c9fafea08505de
Susan Gakii <susangakii71@gmail.com>
2025-10-17 09:26:52 +0300

    react router

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+/* define the NavBar component */
+function NavBar() {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="nav-link"
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="nav-link"
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/profile/:id"
+        className="nav-link"
+      >
+        Profile
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
